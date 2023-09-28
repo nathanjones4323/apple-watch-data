03732c0f92ddddc7ce84384a2741a90bd9df2d35
Nathan Jones <nathanjones4323@gmail.com>
2023-09-27 22:04:55 -0700

    Added create_questions.py to the MB backend initialization

--- metabase/backend/docker-entrypoint-initdb.d/create_questions.py
+++ metabase/backend/docker-entrypoint-initdb.d/create_questions.py
@@ -6,7 +6,8 @@ from loguru import logger
 
 from db.utils import init_metabase_db_connection
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '...', '.env')
+logger.debug(f"dotenv_path: {dotenv_path}")
 try:
     load_dotenv(dotenv_path)
     logger.success("Loaded .env file")
@@ -17,12 +18,20 @@ def create_strong_questions(csv_path):
     try:
         # Read in CSV
         df = pd.read_csv(csv_path)
-        df["id"] = df.index + 1
+    except:
+        logger.error("Could not read Strong data from CSV")
+    try:
         conn = init_metabase_db_connection()
+    except:
+        logger.error("Could not connect to metabase DB")
+    try:
+        df["id"] = df.index + 1
         # Write to Database
         df.to_sql(name="z_report_card_test", con=conn, schema="public", if_exists="replace", index=False)
+        logger.success("Created Strong questions in metabase")
         # Close out DB connection
         conn.close()
     except:
         logger.error("Could not create strong questions in metabase")
 
+create_strong_questions("./metabase/init/strong_questions.csv")
