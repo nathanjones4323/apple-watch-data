ad265a10c43f000bfb772b3d12bf398b55af8f3f
Nathan Jones <nathanjones4323@gmail.com>
2023-10-22 22:47:41 -0700

    Sensible default paths and add start date filter for files

--- datapipelines/extract.py
+++ datapipelines/extract.py
@@ -4,7 +4,7 @@ import pandas as pd
 from loguru import logger
 
 
-def extract_apple_health_data(path) -> pd.DataFrame:
+def extract_apple_health_data(path: str = "./data/apple_health_export/export.xml", start_date: str = None) -> pd.DataFrame:
     # create element tree object
     tree = ET.parse(path)
 
@@ -15,6 +15,9 @@ def extract_apple_health_data(path) -> pd.DataFrame:
     # create DataFrame from a list (rows) of dictionaries (columns)
     try:
         data = pd.DataFrame(record_list)
+        # Filter by start date if provided
+        if start_date:
+            data = data[data['startDate'] >= start_date]
         logger.success("Created DataFrame from Apple Health XML file")
         logger.debug(f"Shape of DataFrame: {data.shape}")
         logger.debug(f"Data: {data}")
@@ -23,9 +26,11 @@ def extract_apple_health_data(path) -> pd.DataFrame:
     return data
 
 
-def extract_strong_app_data(path) -> pd.DataFrame:
+def extract_strong_app_data(path: str = "./data/strong_export/strong.csv", start_date: str = None) -> pd.DataFrame:
     try:
         data = pd.read_csv(path)
+        if start_date:
+            data = data[data['Date'] >= start_date]
         logger.success("Created DataFrame from Strong CSV file")
         logger.debug(f"Shape of DataFrame: {data.shape}")
         logger.debug(f"Data: {data}")
